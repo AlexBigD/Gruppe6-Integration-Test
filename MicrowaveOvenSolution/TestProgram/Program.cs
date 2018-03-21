7f0b0cf3eda60c0be7f5706ffd5e10bb2745c1d2
Søren Mathiasen <zuperzam@email.dk>
2018-03-21 17:53:36 +0100

    Added main program

---
+++ MicrowaveOvenSolution/TestProgram/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+
+namespace Microwave.TestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setup all the objects,
+            var output = new Output();
+            var door = new Door();
+            var pButton = new Button();
+            var tButton = new Button();
+            var scButton = new Button();
+            var display = new Display(output);
+            var light = new Light(output);
+            var timer = new Timer();
+            var pTube = new PowerTube(output);
+            var CController = new CookController(timer, display, pTube);
+            var UI = new UserInterface(tButton, tButton, scButton, door, display, light, CController);
+
+            // Simulate user activities
+            door.Open();
+            door.Close();
+            pButton.Press();
+            tButton.Press();
+            tButton.Press();
+            scButton.Press();
+            door.Open();
+
+            
+
+
+
+            // Wait while the classes, including the timer, do their job
+            System.Console.WriteLine("Tast enter når applikationen skal afsluttes");
+            System.Console.ReadLine();
+        }
+    }
+}
